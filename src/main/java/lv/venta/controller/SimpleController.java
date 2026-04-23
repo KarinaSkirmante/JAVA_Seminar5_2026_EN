7d9d4d085c5506d6db2e0fa7370e05797c9c2266
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-23 11:03:36 +0300

    Add: added new controller class with new simple controller and simple-page.html file as vies

---
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -0,0 +1,14 @@
+package lv.venta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+	
+	@GetMapping("/simple")//localhost:8080/simple
+	public String getControllerSimple() {
+		System.out.println("The simple controller is running");
+		return "simple-page";//will show simple-page.html file
+	}
+}
