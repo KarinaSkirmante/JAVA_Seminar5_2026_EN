699ccd679b5516dc691b96701a24d96aa711c063
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-24 15:23:41 +0300

    Edit: added enum as string in the database

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -2,6 +2,8 @@ package lv.venta.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,6 +26,7 @@ public class Product {
 	
 	@NotNull
 	@Column(name = "Category")
+	@Enumerated(EnumType.STRING)
 	private Category category;
 	
 	@Min(0)
