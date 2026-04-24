69959d22c067ba1ab5aa30efa5cc93561ca32f38
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-24 14:51:11 +0300

    Edit: added comandlinerunner function for test data saving in the repository

--- src/main/java/lv/venta/Seminar5Application.java
+++ src/main/java/lv/venta/Seminar5Application.java
@@ -1,13 +1,46 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.model.Category;
+import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class Seminar5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar5Application.class, args);
+	}
+	
+	@Bean// it will call this function automatically
+	public CommandLineRunner testDatabase(IProductRepo prodRepo) {
+		
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Product productData1 = 
+						new Product(2, "Apple", Category.fruits, 0.99f, 3, "Red");
+				
+				Product productData2 = 
+							new Product(1, "Grapes", Category.fruits, 1.99f, 2, "Green");
+
+				Product productData3 = 
+							new Product(3, "Watermelon", Category.fruits, 4.99f, 1, "Sweet");
+
+				prodRepo.save(productData1);
+				prodRepo.save(productData2);
+				prodRepo.save(productData3);
+				
+				
+			}
+		};
+		
+		
 	}
 
 }
