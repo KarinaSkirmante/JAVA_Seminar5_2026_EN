465a284bc7e12b165cf028460847e46a27fb910f
KarinaSkirmante <karina.krinkele@gmail.com>
2026-05-07 09:52:01 +0300

    Edit: finished CRUD service class for Product

--- src/main/java/lv/venta/repo/IProductRepo.java
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -2,9 +2,13 @@ package lv.venta.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import lv.venta.model.Category;
 import lv.venta.model.Product;
 
 public interface IProductRepo 
 		extends CrudRepository<Product, Integer> {
+
+	boolean existsByTitleAndCategoryAndPriceAndQuantityAndDescription(String title, Category category, float price,
+			int quantity, String description);
 	//only abstract functions will be there
 }
--- src/main/java/lv/venta/service/impl/ProductCRUDServiceImpl.java
+++ src/main/java/lv/venta/service/impl/ProductCRUDServiceImpl.java
@@ -19,7 +19,28 @@ public class ProductCRUDServiceImpl implements IProductCRUDService{
 	@Override
 	public void createProduct(String title, Category category, float price, int quantity, String description)
 			throws Exception {
-		// TODO Auto-generated method stub
+		if(title == null 
+			|| title.isEmpty() 
+			|| !title.matches("[A-Z]{1}[a-z ]{2,40}")
+			|| category == null
+			|| price < 0
+			|| price > 100
+			|| quantity < 0
+			|| quantity > 1000
+			|| description == null
+			|| description.isEmpty()) {
+			throw new Exception("Input data is incorrect");
+		}
+		
+		if(prodRepo
+			.existsByTitleAndCategoryAndPriceAndQuantityAndDescription
+			(title, category, price, quantity, description )) {
+			throw new Exception("This product already exists in the DB");
+		}
+		
+		Product prod = new Product(title, category, price, quantity, description);
+		prodRepo.save(prod);
+		
 		
 	}
 
@@ -39,20 +60,49 @@ public class ProductCRUDServiceImpl implements IProductCRUDService{
 
 	@Override
 	public Product retrieveProductById(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(id <= 0) {
+			throw new Exception("Id should be positive");
+		}
+		
+		if(!prodRepo.existsById(id)) {
+			throw new Exception("Product with id " + id + " doesn't exist");
+		}
+		
+		
+		return prodRepo.findById(id).get();
 	}
 
 	@Override
 	public void updateProductById(int id, String title, Category category, float price, int quantity,
 			String description) throws Exception {
-		// TODO Auto-generated method stub
+		if(title == null 
+				|| title.isEmpty() 
+				|| !title.matches("[A-Z]{1}[a-z ]{2,40}")
+				|| category == null
+				|| price < 0
+				|| price > 100
+				|| quantity < 0
+				|| quantity > 1000
+				|| description == null
+				|| description.isEmpty()) {
+				throw new Exception("Input data is incorrect");
+			}
+		
+		Product productFromDB = retrieveProductById(id);
+		productFromDB.setTitle(title);
+		productFromDB.setCategory(category);
+		productFromDB.setPrice(price);
+		productFromDB.setQuantity(quantity);
+		productFromDB.setDescription(description);
+		
+		prodRepo.save(productFromDB);
 		
 	}
 
 	@Override
 	public void deleteProductById(int id) throws Exception {
-		// TODO Auto-generated method stub
+		Product productFromDB = retrieveProductById(id);
+		prodRepo.delete(productFromDB);
 		
 	}
 
