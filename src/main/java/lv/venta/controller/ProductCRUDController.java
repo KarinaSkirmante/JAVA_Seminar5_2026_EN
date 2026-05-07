be7eeee1eff214152948429e8dd4e103261a349f
KarinaSkirmante <karina.krinkele@gmail.com>
2026-05-07 10:47:29 +0300

    Edit: added get product by id controller the second variant with ?id= in ProductCRUDController

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.model.Product;
 import lv.venta.service.IProductCRUDService;
@@ -51,6 +52,21 @@ public class ProductCRUDController {
 			return "error-page";
 		}
 		
+	}
+	
+	@GetMapping("/one")//localhost:8080/product/crud/one?id=2
+	public String getControllerToGetOneProductById2
+	(@RequestParam(name = "id") int id, Model model) {
+		try
+		{
+		Product productFromDB = prodService.retrieveProductById(id);
+		model.addAttribute("box", productFromDB);
+		return "product-page";
+		}
+		catch (Exception e) {
+			model.addAttribute("box", e.getMessage());
+			return "error-page";
+		}
 	}
 
 }
